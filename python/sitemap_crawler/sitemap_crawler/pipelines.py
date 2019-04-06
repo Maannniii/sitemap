eb000fad68ad3506e4e470c71e89f31197ea8c23
Mani M <mani@cogknit.com>
2019-04-06 18:13:13 +0530

    reformatted code

--- python/sitemap_crawler/sitemap_crawler/pipelines.py
+++ python/sitemap_crawler/sitemap_crawler/pipelines.py
@@ -1,29 +1,31 @@
 # -*- coding: utf-8 -*-
 
+from json import dumps
+
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import pymysql
-from json import dumps
 from sitemap_crawler import settings
+
 
 class SitemapCrawlerPipeline(object):
     def __init__(self):
-        self.connection=pymysql.connect(settings.DB_HOST,settings.DB_USER,settings.DB_PASSWORD,settings.DB_NAME)
-        self.cursor=self.connection.cursor(pymysql.cursors.DictCursor)
+        self.connection = pymysql.connect(settings.DB_HOST, settings.DB_USER, settings.DB_PASSWORD, settings.DB_NAME)
+        self.cursor = self.connection.cursor(pymysql.cursors.DictCursor)
         self.cursor.execute("truncate sitemap")
         self.cursor.execute("alter table sitemap auto_increment=1")
         self.connection.commit()
 
-
     def process_item(self, item, spider):
         sql = "insert into sitemap(url,external_urls,static_urls,urls) values('{}','{}','{}','{}')"
         try:
-            formatted_sql=sql.format(dumps(item['url']),dumps(item['external_urls']),dumps(item['static_urls']),dumps(item['urls']))
+            formatted_sql = sql.format(dumps(item['url']), dumps(item['external_urls']), dumps(item['static_urls']),
+                                       dumps(item['urls']))
             self.cursor.execute(formatted_sql)
         except Exception as e:
-            print ("Exception occured",e)
+            print("Exception occured", e)
         return item
 
     def close_spider(self, spider):
--- python/sitemap_crawler/sitemap_crawler/spiders/Sitemapcrawler.py
+++ python/sitemap_crawler/sitemap_crawler/spiders/Sitemapcrawler.py
@@ -1,36 +1,42 @@
 # -*- coding: utf-8 -*-
-import scrapy,re
-from urllib.parse import urlparse,urljoin
+from urllib.parse import urlparse, urljoin
+
+import re
+import scrapy
 from sitemap_crawler.items import SitemapCrawlerItem as Item
 from sitemap_crawler.settings import CRAWL_START_URL
+
+
 class SitemapcrawlerSpider(scrapy.Spider):
     name = 'Sitemapcrawler'
     allowed_domains = [CRAWL_START_URL]
     start_urls = [CRAWL_START_URL]
     whitespace_pattern = r"""[\n\t\r\x0b\x0c'"]*"""
-    url_details=urlparse(CRAWL_START_URL)
+    url_details = urlparse(CRAWL_START_URL)
 
     def parse(self, response):
-        item=Item()
-        base_url=self.clean(response.url.strip())
-        static_urls=set(response.xpath("//*/@src").getall())
-        junk_urls=set(response.xpath("//script/@src").getall())
-        item['static_urls']=[urljoin(base_url,x) for x in map(self.clean,static_urls.difference(junk_urls)) if bool(x)]
-        item['url']=base_url
-        urls=set(response.xpath('//*/@href').getall())
-        urls=set(map(self.remove_same_pages,urls))
-        external_urls=set(filter(lambda x: urlparse(x).netloc!=self.url_details.netloc ,urls))
-        urls=[urljoin(base_url,x) for x in map(self.clean,urls.difference(external_urls).difference({response.url,CRAWL_START_URL})) if bool(x)]
-        urls=[] if len(urls)==0 else [urls[0]]
-        external_urls=[urljoin(base_url,x) for x in map(self.clean,external_urls) if bool(x)]
-        item['external_urls']=external_urls
+        item = Item()
+        base_url = self.clean(response.url.strip())
+        static_urls = set(response.xpath("//*/@src").getall())
+        junk_urls = set(response.xpath("//script/@src").getall())
+        item['static_urls'] = [urljoin(base_url, x) for x in map(self.clean, static_urls.difference(junk_urls)) if
+                               bool(x)]
+        item['url'] = base_url
+        urls = set(response.xpath('//*/@href').getall())
+        urls = set(map(self.remove_same_pages, urls))
+        external_urls = set(filter(lambda x: urlparse(x).netloc != self.url_details.netloc, urls))
+        urls = [urljoin(base_url, x) for x in
+                map(self.clean, urls.difference(external_urls).difference({response.url, CRAWL_START_URL})) if bool(x)]
+        urls = [] if len(urls) == 0 else [urls[0]]
+        external_urls = [urljoin(base_url, x) for x in map(self.clean, external_urls) if bool(x)]
+        item['external_urls'] = external_urls
         item['urls'] = urls
         for url in urls:
             yield response.follow(url, callback=self.parse)
         yield item
-        
-    def clean(self,data):
-         return re.sub(self.whitespace_pattern,'',data).strip()
 
-    def remove_same_pages(self,x):
-         return x.split('#')[0]
+    def clean(self, data):
+        return re.sub(self.whitespace_pattern, '', data).strip()
+
+    def remove_same_pages(self, x):
+        return x.split('#')[0]
